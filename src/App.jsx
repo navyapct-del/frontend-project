cc40948ca1bc4c3e0a770b16eeb0e57c97332e72
NavyaP <navyap@cloudthat.com>
2026-04-28 11:52:56 +0530

    remove again

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,3 @@
-// import ScrollToTop from "@/base-components/scroll-to-top/Main";
-// import { BrowserRouter } from "react-router-dom";
-// import { RecoilRoot } from "recoil";
-// import Router from "./router";
-
-// function App() {
-//   return (
-//     <>
-//       <RecoilRoot>
-//         <BrowserRouter>
-//           <Router />
-//           <ScrollToTop />
-//         </BrowserRouter>
-//       </RecoilRoot>
-
-//       <button
-//         style={{
-//           padding: "10px 20px",
-//           backgroundColor: "blue",
-//           color: "white",
-//           border: "none",
-//           borderRadius: "5px",
-//           margin: "10px"
-//         }}
-//         onClick={() => alert("Test button working")}
-//       >
-//         Test Button
-//       </button>
-//     </>
-//   );
-// }
-// // function App() {
-// //   return (
-// //     <RecoilRoot>
-// //       <BrowserRouter>
-// //         <Router />
-// //         <ScrollToTop />
-// //       </BrowserRouter>
-// //     </RecoilRoot>
-// //  
-// //   );
-// // }
-
-// // export default App;
-
 import ScrollToTop from "@/base-components/scroll-to-top/Main";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
@@ -50,32 +5,14 @@ import Router from "./router";
 
 function App() {
   return (
-    <>
-      <RecoilRoot>
-        <BrowserRouter>
-          <Router />
-          <ScrollToTop />
-        </BrowserRouter>
-      </RecoilRoot>
-
-      <button
-        style={{
-          position: "fixed",
-          top: "20px",
-          right: "20px",
-          zIndex: 9999,
-          padding: "10px 20px",
-          backgroundColor: "blue",
-          color: "white",
-          border: "none",
-          borderRadius: "5px"
-        }}
-        onClick={() => alert("Test button working")}
-      >
-        Test Button
-      </button>
-    </>
+    <RecoilRoot>
+      <BrowserRouter>
+        <Router />
+        <ScrollToTop />
+      </BrowserRouter>
+    </RecoilRoot>
   );
 }
 
 export default App;
+
